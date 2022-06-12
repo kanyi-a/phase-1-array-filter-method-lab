f47e988d4358d2537b51fa5f6f982b26615a4609
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-06-12 11:40:44 +0300

    Done assignment

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
 function findMatching(array, name) {
   return array.filter((match) => match.toLowerCase() === name.toLowerCase());
 }
+const fuzzyMatch = (array, names) => {
+    let newArray = [];
+    for (const name of array) {
+      if (name.slice(0, 1) === names.slice(0, 1)) {
+        newArray.push(name);
+      }
+    }
+    return newArray;
+  };
+  
+  const matchName = (array, str) => {
+    const newArray = [];
+    for (const user of array) {
+      if (
+        user.name.toLowerCase() === str.toLowerCase() ||
+        user.hometown.toLowerCase() === str.toLowerCase()
+      ) {
+        newArray.push(user);
+      }
+    }
+    return newArray;
+  };
