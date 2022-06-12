35c8d687c976cb6dfaf60c96a8fdadbb7dca09bd
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-06-12 11:39:45 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(array, name) {
+  return array.filter((match) => match.toLowerCase() === name.toLowerCase());
+}
